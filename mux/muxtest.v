705fdc34a4e75a2794ee62f17e751ce94d903489
Adithi <adithi.upadhya710@gmail.com>
2023-02-23 23:14:48 +0530

    minor changes

--- mux/muxtest.v
+++ mux/muxtest.v
@@ -1,8 +1,13 @@
+`include "mux16to1.v"
+`include "mux4to1.v"
+`include "mux2to1.v"
 module muxtest;
 	reg[15:0]A; reg[3:0]S; wire F;
 	mux16to1 M(.in(A),.sel(S),.out(F));
 	initial
 	 begin
+		$dumpfile("muxtest.vcd");
+        $dumpvars(0,muxtest);
 		$monitor($time,"A=%h, S=%h, F=%b",A,S,F);
 		#5 A=16'h3f0a; S=4'h0;
 		#5 S=4'h1;
@@ -10,4 +15,5 @@ module muxtest;
 		#5 S=4'hc;
 		#5 $finish;
 	 end
+
 endmodule
